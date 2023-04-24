e20b288331b9bb364faa8e3c696510f324a66c94
v15hv4 <vishva2912@gmail.com>
2023-04-24 18:26:59 +0530

    Implement manage club page

---
+++ pages/manage/clubs/@mine.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+import { useRouter } from "next/router";
+
+import { Box, Card, Container } from "@mui/material";
+
+import { useAuth } from "contexts/AuthContext";
+
+import Page from "components/Page";
+import ClientOnly from "components/ClientOnly";
+import { ClubHero } from "components/clubs";
+import { RichTextEditor } from "components/RichTextEditor";
+
+import { useQuery } from "@apollo/client";
+import { GET_CLUB } from "gql/queries/clubs";
+
+export default function Club() {
+    const {
+        query: { id },
+    } = useRouter();
+
+    // set title asynchronously
+    const [title, setTitle] = useState("...");
+
+    return (
+        <Page title={title}>
+            <Container>
+                <ClientOnly>
+                    <ClubDetails cid={id} setTitle={setTitle} />
+                </ClientOnly>
+            </Container>
+        </Page>
+    );
+}
+
+function ClubDetails({ cid, setTitle }) {
+    const { user } = useAuth();
+
+    const {
+        data: { club } = {},
+        loading,
+        error,
+    } = useQuery(GET_CLUB, {
+        skip: !user?.uid,
+        variables: {
+            clubInput: { cid: user?.uid },
+        },
+        onCompleted: ({ club }) => {
+            setTitle(club?.name);
+        },
+    });
+
+    // TODO: handle loading screen and non-existent club
+    return loading ? null : !club ? null : (
+        <Card sx={{ mb: 4 }}>
+            <ClubHero club={club} />
+            <Box sx={{ p: { xs: 3, md: 5 } }}>
+                <RichTextEditor
+                    editing={false}
+                    editorState={[JSON.parse(club.description), null]}
+                />
+            </Box>
+        </Card>
+    );
+}
