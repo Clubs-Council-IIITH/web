a7669a81a89c7390861dd271d25eaa587fd29cc2
v15hv4 <vishva2912@gmail.com>
2023-04-28 18:51:03 +0530

    Fix calendar

--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -1,29 +1,36 @@
 import moment from "moment";
-import { useContext, useState, useEffect } from "react";
-import { Container } from "@mui/material";
-import { useQuery } from "@apollo/client";
+
+import { useState, useEffect } from "react";
+
 import { Calendar, momentLocalizer } from "react-big-calendar";
-import Page from "components/Page";
-import { GET_ALL_EVENTS } from "gql/queries/events.jsx";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { useQuery } from "@apollo/client";
+import { GET_ALL_EVENTS } from "gql/queries/events.jsx";
+
+import { Container } from "@mui/material";
+import Page from "components/Page";
 
 const EventCalendar = () => {
     const localizer = momentLocalizer(moment);
-    const { data, loading } = useQuery(GET_ALL_EVENTS, {
-        pollInterval: 1000 * 60 * 5, // 5 minutes
-    });
     const [events, setEvents] = useState([]);
-    useEffect(() => {
-        setEvents(
-            data?.allEvents?.map((e) => ({
-                start: moment(e.datetimeStart).toDate(),
-                end: moment(e.datetimeEnd).toDate(),
-                title: e.name,
-            })) || []
-        );
-    }, [data]);
 
-    return (
+    const { loading } = useQuery(GET_ALL_EVENTS, {
+        variables: {
+            clubid: null,
+        },
+        onCompleted: ({ allEvents }) => {
+            setEvents(
+                allEvents?.map((e) => ({
+                    start: moment(e.datetimeperiod[0]).toDate(),
+                    end: moment(e.datetimeperiod[1]).toDate(),
+                    title: e.name,
+                })) || []
+            );
+        },
+    });
+
+    return loading ? null : (
         <Page title="Calendar">
             <Container>
                 <Calendar
