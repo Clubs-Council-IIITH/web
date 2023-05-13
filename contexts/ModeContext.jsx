3810baa6845910bb1cd5080f6008c9a141fd091e
Bhav Beri <bhavberi@gmail.com>
2023-05-13 16:26:57 +0530

    add persistent mode in localstorage

--- contexts/ModeContext.jsx
+++ contexts/ModeContext.jsx
@@ -1,8 +1,8 @@
-import { createContext, useState, useContext, useEffect } from "react";
+import { createContext, useState, useContext, useEffect, useReducer } from "react";
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 export const ModeContext = createContext({
-    isLight: true,
+    isLight: null,
     trackProgress: () => null,
 });
 
@@ -10,17 +10,45 @@ export function useMode() {
     return useContext(ModeContext);
 }
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'true': {
+            return {
+                mode: true
+            };
+        }
+        case 'false': {
+            return {
+                mode: false
+            };
+        }
+    }
+    throw Error('Unknown action: ' + action.type);
+}
+
 export function ModeProvider({ children }) {
     const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-    const [isLight, setMode] = useState(!prefersDarkMode);
+    const [state, dispatch] = useReducer(reducer, { mode: !prefersDarkMode });
+    const [isLight, setIsLight] = useState(!prefersDarkMode);
 
     useEffect(() => {
-        setMode(!prefersDarkMode);
-    }, [prefersDarkMode]);
+        let theme = localStorage.getItem("cc-theme");
+        if (theme != null)
+        dispatch({ type: theme.toString() });
+    }, []);
+
+    const setMode = (mode) => {
+        dispatch({ type: mode.toString() });
+        localStorage.setItem("cc-theme", mode);
+    };
 
     const changeMode = () => {
         setMode(!isLight);
     };
+
+    useEffect(() => {
+        setIsLight(state.mode);
+    }, [state]);
 
     return (
         <ModeContext.Provider value={{ isLight, changeMode, setMode }}>
