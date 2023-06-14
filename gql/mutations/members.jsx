5fc430429a1bc4361453d671ec3de57d6a123927
Bhav Beri <bhavberi@gmail.com>
2023-06-14 22:11:53 +0530

    Added all members mutations

--- gql/mutations/members.jsx
+++ gql/mutations/members.jsx
@@ -7,3 +7,27 @@ export const CREATE_MEMBER = gql`
         }
     }
 `;
+
+export const EDIT_MEMBER = gql`
+    mutation EditMember($memberInput: FullMemberInput!) {
+        editMember(memberInput: $memberInput) {
+            _id
+        }
+    }
+`;
+
+export const DELETE_MEMBER = gql`
+    mutation DeleteMember($memberInput: SimpleMemberInput!) {
+        deleteMember(memberInput: $memberInput) {
+            _id
+        }
+    }
+`;
+
+export const APPROVE_MEMBER = gql`
+    mutation ApproveMember($memberInput: SimpleMemberInput!) {
+        approveMember(memberInput: $memberInput) {
+            _id
+        }
+    }
+`;
