66a914185fe5e68f48134526781f53913038bac4
Bhav Beri <bhavberi@gmail.com>
2023-06-18 10:03:07 +0530

    Fix 401 and 404 pages

--- pages/401.jsx
+++ pages/401.jsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from "@mui/material";
-import Image from "next/image";
 
 const Error401SVG = "/assets/vector/401.svg";
 
@@ -15,7 +14,7 @@ export default function Custom401() {
             justifyContent="center"
         >
             <Box mb={3}>
-                <Image
+                <img
                     src={Error401SVG}
                     alt="401: Unauthorized"
                     width="300px"
