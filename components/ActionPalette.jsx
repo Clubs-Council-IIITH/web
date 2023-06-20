7405f1e86ba3c2b991344054c6c13f4a61d76e6c
Bhav Beri <bhavberi@gmail.com>
2023-06-21 02:09:48 +0530

    Updated ActionPalette to support passing of items forward

--- components/ActionPalette.jsx
+++ components/ActionPalette.jsx
@@ -2,27 +2,27 @@ import { useState } from "react";
 
 import { Grid, Box } from "@mui/material";
 
-export default function ActionPalette({ actions = [] }) {
+export default function ActionPalette({ actions = [], ...props }) {
     const [view, setView] = useState("base");
 
     return {
-        base: <BaseView actions={actions} setView={setView} />,
+        base: <BaseView actions={actions} setView={setView} {...props} />,
         ...Object.assign(
             {},
             ...actions
                 ?.filter((a) => a.view !== null)
-                ?.map((a) => ({ [a.name]: <a.view setView={setView} /> }))
+                ?.map((a) => ({ [a.name]: <a.view setView={setView} {...props} /> }))
         ),
     }[view];
 }
 
-function BaseView({ actions, setView }) {
+function BaseView({ actions, setView, ...props }) {
     return (
         <Box sx={{ border: 1, borderRadius: 1, borderColor: "grey.300", borderStyle: "dashed" }}>
             <Grid container spacing={2} p={2}>
                 {actions?.map((Action, key) => (
                     <Grid item xs key={key}>
-                        <Action.button setView={setView} />
+                        <Action.button setView={setView} {...props} />
                     </Grid>
                 ))}
             </Grid>
