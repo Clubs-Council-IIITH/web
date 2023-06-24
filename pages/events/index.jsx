e63feac1d482cfb3823a556eb8bfe5b9d7ee6a16
v15hv4 <vishva2912@gmail.com>
2023-06-25 02:50:14 +0530

    Add filters to events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,150 +1,189 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
-import { Box, Card, Container, InputAdornment, TextField, TableRow, TableCell, Typography } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  Grid,
+  Container,
+  Select,
+  MenuItem,
+  InputLabel,
+  InputAdornment,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 import { useProgressbar } from "contexts/ProgressbarContext";
 
 import Page from "components/Page";
-import Table from "components/Table";
-import { ClubBanner } from "components/clubs";
+import { EventCard } from "components/events";
 import ClientOnly from "components/ClientOnly";
 
 import { useQuery } from "@apollo/client";
+import { GET_ALL_CLUB_IDS } from "gql/queries/clubs";
 import { GET_ALL_EVENTS } from "gql/queries/events";
-import { GET_CLUB } from "gql/queries/clubs";
-
-import { fDateTime } from "utils/formatTime";
 
 export default function Events() {
-    // Get all Events
-    const { loading, error, data: { events } = {} } = useQuery(GET_ALL_EVENTS, {
-        variables: {
-            clubid: null,
-        }
-    });
+  const router = useRouter();
 
-    const [filteredEvents, setFilteredEvents] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
+  // Get all Events
+  const {
+    loading,
+    error,
+    data: { events } = {},
+  } = useQuery(GET_ALL_EVENTS, {
+    variables: {
+      clubid: null,
+    },
+    onCompleted: console.log,
+  });
 
-    useEffect(() => {
-        const filteredRows = events?.filter((event) =>
-            event?.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // get filters from query params
+  const { club: clubFilter, state: stateFilter } = router.query;
+
+  useEffect(() => {
+    if (events) {
+      var filteredRows = events;
+
+      // filter by club
+      if (clubFilter) {
+        filteredRows = filteredRows.filter(
+          (event) => event?.clubid === clubFilter
         );
-        setFilteredEvents(filteredRows);
-    }, [searchTerm, events]);
+      }
 
-    const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
+      // filter by state
+      if (stateFilter === "upcoming") {
+        filteredRows = filteredRows.filter(
+          (event) => new Date(event?.datetimeperiod[1]) > new Date()
+        );
+      } else if (stateFilter === "completed") {
+        filteredRows = filteredRows.filter(
+          (event) => new Date(event?.datetimeperiod[1]) <= new Date()
+        );
+      }
 
-    // track loading state
-    const { trackProgress } = useProgressbar();
-    useEffect(() => trackProgress(loading), [loading]);
+      // filter by search term
+      if (searchTerm) {
+        filteredRows = filteredRows.filter((event) =>
+          event?.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      }
 
-    return (
-        <Page title="All Events">
-            <Container>
-                <center>
-                    <Typography variant="h2" sx={{ mb: 4 }}>
-                        All Events
-                    </Typography>
-                </center>
+      setFilteredEvents(filteredRows);
+    }
+  }, [events, searchTerm, clubFilter, stateFilter]);
 
-                <TextField
-                    id="search"
-                    type="search"
-                    label="Search"
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                    fullWidth
-                    disabled={loading}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <SearchIcon />
-                            </InputAdornment>
-                        ),
-                    }}
-                />
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-                <Card>
-                    <Box m={1}>
-                        <ClientOnly>
-                            {loading ? null : !events?.length ? null : (
-                                <Table
-                                    data={filteredEvents}
-                                    header={EventsTableHeader}
-                                    row={EventsTableRow}
-                                    noDataMessage="No Search Results Found!"
-                                />
-                            )}
-                        </ClientOnly>
-                    </Box>
-                </Card>
-            </Container>
-        </Page>
-    );
-}
+  // populate club IDs if user is CC
+  const { data: { allClubs: clubs } = {}, loading: clubsLoading } = useQuery(
+    GET_ALL_CLUB_IDS,
+    {}
+  );
 
-function EventsTableHeader() {
-    return (
-        <TableRow>
-            <TableCell align="left">Name</TableCell>
-            <TableCell align="left">Club</TableCell>
-            <TableCell align="left">Scheduled</TableCell>
-        </TableRow>
-    );
-}
+  // track loading state
+  const { trackProgress } = useProgressbar();
+  useEffect(() => trackProgress(loading), [loading]);
 
-function EventsTableRow(event) {
-    const router = useRouter();
-    const { name, datetimeperiod } = event;
+  return (
+    <Page title="All Events">
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              id="search"
+              type="search"
+              label="Search by name"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              fullWidth
+              disabled={loading}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
 
-    // get club
-    const {
-        loading: clubLoading,
-        error: clubError,
-        data: { club } = {},
-    } = useQuery(GET_CLUB, {
-        skip: !event?.clubid,
-        variables: {
-            clubInput: { cid: event?.clubid },
-        },
-    });
-
-    return (
-        <TableRow
-            hover
-            onClick={() => router.push(`/events/${event._id}`)}
-            sx={{ cursor: "pointer" }}
-        >
-            <TableCell
-                align="left"
-                sx={{ border: "none" }}
-                style={{ maxWidth: 450 }}
-            >
-                <Typography variant="subtitle2" noWrap>
-                    {name}
-                </Typography>
-            </TableCell>
-            <TableCell
-                align="left"
-                sx={{ border: "none" }}
-                style={{ maxWidth: 250 }}
-            >
-                { clubLoading ? null : clubError ? null :
-                    <ClubBanner {...club} border={false} onclick={false} />
+          <Grid item xs={12} md={8}>
+            <FormControl sx={{ width: "100%" }}>
+              <InputLabel id="clubSelect">Filter by club</InputLabel>
+              <Select
+                id="clubSelect"
+                labelId="clubSelect"
+                input={<OutlinedInput label="Filter by club" />}
+                value={clubFilter || ""}
+                onChange={(e) =>
+                  router.replace({
+                    query: { ...router.query, club: e.target.value },
+                  })
                 }
-            </TableCell>
-            <TableCell align="left" sx={{ border: "none" }}>
-                <Box display="flex" alignItems="center">
-                    {fDateTime(datetimeperiod?.[0], "D MMM, H:mm")}
-                    <Box mx={1}>-</Box>
-                    {fDateTime(datetimeperiod?.[1], "D MMM, H:mm")}
-                </Box>
-            </TableCell>
-        </TableRow >
-    );
+              >
+                {clubs
+                  ?.slice()
+                  ?.sort((a, b) => a.name.localeCompare(b.name))
+                  ?.map((club) => (
+                    <MenuItem key={club?.cid} value={club?.cid}>
+                      {club?.name}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs md>
+            <FormControl sx={{ width: "100%" }}>
+              <InputLabel id="stateSelect">State</InputLabel>
+              <Select
+                id="stateSelect"
+                labelId="stateSelect"
+                input={<OutlinedInput label="State" />}
+                defaultValue={"all"}
+                onChange={(e) =>
+                  router.replace({
+                    query: { ...router.query, state: e.target.value },
+                  })
+                }
+              >
+                <MenuItem value={"all"}>All</MenuItem>
+                <MenuItem value={"upcoming"}>Upcoming</MenuItem>
+                <MenuItem value={"completed"}>Completed</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <Box pt={2}>
+          <ClientOnly>
+            {loading ? null : !events?.length ? null : (
+              <EventsGrid data={filteredEvents} />
+            )}
+          </ClientOnly>
+        </Box>
+      </Container>
+    </Page>
+  );
+}
+
+function EventsGrid({ data }) {
+  return (
+    <Grid container spacing={2}>
+      {/* display only 3/4 events on the main page */}
+      {data?.map((event, key) => (
+        <Grid key={key} item xs={6} sm={4} md={3}>
+          <EventCard event={event} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
