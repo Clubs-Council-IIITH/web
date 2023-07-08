ee968ac0c5792991a3121d3d86b6875c45e1c948
v15hv4 <vishva2912@gmail.com>
2023-07-08 16:28:18 +0530

    Sort audience chips

--- components/events/AudienceChips.jsx
+++ components/events/AudienceChips.jsx
@@ -4,11 +4,16 @@ import { audienceLabels } from "utils/formatEvent";
 export default function AudienceChips({ audience }) {
   return (
     <Grid container spacing={1}>
-      {audienceLabels(audience)?.map(({ name, color }) => (
-        <Grid item key={name}>
-          <Chip label={name} sx={{ color: `${color}.dark`, backgroundColor: `${color}.lighter` }} />
-        </Grid>
-      ))}
+      {audienceLabels(audience)
+        ?.sort((a, b) => a.name.localeCompare(b.name))
+        ?.map(({ name, color }) => (
+          <Grid item key={name}>
+            <Chip
+              label={name}
+              sx={{ color: `${color}.dark`, backgroundColor: `${color}.lighter` }}
+            />
+          </Grid>
+        ))}
     </Grid>
   );
 }
