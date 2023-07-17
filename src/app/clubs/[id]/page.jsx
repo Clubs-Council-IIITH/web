580432c7f553e2bdfd7213cb90614ed093c87b18
v15hv4 <vishva2912@gmail.com>
2023-07-18 02:45:12 +0530

    Add event grid to club view

--- src/app/clubs/[id]/page.jsx
+++ src/app/clubs/[id]/page.jsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 import { getClient } from "gql/client";
 import { GET_CLUB } from "gql/queries/clubs";
 
-import { Box, Card } from "@mui/material";
+import { Divider, Stack, Button, Box, Card, Typography } from "@mui/material";
+
+import Icon from "components/Icon";
+
 import ClubBanner from "components/clubs/ClubBanner";
 import ClubInfo from "components/clubs/ClubInfo";
 import ClubSocials from "components/clubs/ClubSocials";
+
+import EventsGrid from "components/events/EventsGrid";
 
 export async function generateMetadata({ params }, parent) {
   const { id } = params;
@@ -45,6 +52,31 @@ export default async function Club({ params }) {
         <ClubInfo name={club.name} logo={club.logo} tagline={club.tagline} />
       </Box>
       <ClubSocials socials={club.socials} />
+
+      <Divider sx={{ borderStyle: "dashed", mt: 3 }} />
+
+      <Stack direction="column" mx={2}>
+        <Box my={4}>
+          <Box display="flex" justifyContent="space-between" mb={1}>
+            <Box display="flex" alignItems="center">
+              <Icon variant="local-activity-outline-rounded" mr={1} />
+              <Typography variant="h4">Events</Typography>
+            </Box>
+            <Button
+              variant="none"
+              color="secondary"
+              component={Link}
+              href={`/events?club=${id}`}
+            >
+              <Typography variant="button" color="text.primary">
+                View all
+              </Typography>
+              <Icon variant="chevron-right" />
+            </Button>
+          </Box>
+          <EventsGrid type="club" clubid={id} limit={4} />
+        </Box>
+      </Stack>
     </Box>
   );
 }
