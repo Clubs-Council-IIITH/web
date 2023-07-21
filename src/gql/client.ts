2d3d659b1acfa282bb90c1566399a8d0938df784
v15hv4 <vishva2912@gmail.com>
2023-07-22 01:29:50 +0530

    Pass client cookies to server

--- src/gql/client.ts
+++ src/gql/client.ts
@@ -1,19 +1,35 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { cookies } from "next/headers";
+import { HttpLink } from "@apollo/client";
 import {
   NextSSRInMemoryCache,
   NextSSRApolloClient,
 } from "@apollo/experimental-nextjs-app-support/ssr";
+import { setContext } from "@apollo/client/link/context";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
 const GRAPHQL_ENDPOINT =
   process.env.GRAPHQL_ENDPOINT || "http://gateway/graphql";
 
 export const { getClient } = registerApolloClient(() => {
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        cookie: cookies()
+          .getAll()
+          .map((cookie) => `${cookie.name}=${cookie.value}`)
+          .join("; "),
+      },
+    };
+  });
+
+  const httpLink = new HttpLink({
+    uri: GRAPHQL_ENDPOINT,
+    credentials: "include",
+  });
+
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
-    link: new HttpLink({
-      uri: GRAPHQL_ENDPOINT,
-      credentials: "include",
-    }),
+    link: authLink.concat(httpLink),
   });
 });
