4838728ede2862e144756ea939e8b723ffb07e8c
v15hv4 <vishva2912@gmail.com>
2023-08-08 02:56:35 +0530

    Restructure profile pages and action permissions

--- src/app/profile/[id]/page.jsx
+++ src/app/profile/[id]/page.jsx
@@ -1,10 +1,14 @@
 import { getClient } from "gql/client";
+import { GET_USER } from "gql/queries/auth";
 import { GET_USER_PROFILE } from "gql/queries/users";
 
 import { Container, Grid, Stack, Typography } from "@mui/material";
 
+import ActionPalette from "components/ActionPalette";
+import ClubLogo from "components/clubs/ClubLogo";
 import UserImage from "components/users/UserImage";
 import UserDetails from "components/profile/UserDetails";
+import { EditUser } from "components/profile/UserActions";
 
 export async function generateMetadata({ params }, parent) {
   const { id } = params;
@@ -27,6 +31,13 @@ export async function generateMetadata({ params }, parent) {
 export default async function Profile({ params }) {
   const { id } = params;
 
+  // get currently logged in user
+  const {
+    data: { userMeta: currentUserMeta, userProfile: currentUserProfile } = {},
+  } = await getClient().query(GET_USER, { userInput: null });
+  const currentUser = { ...currentUserMeta, ...currentUserProfile };
+
+  // get target user
   const { data: { userProfile, userMeta } = {} } = await getClient().query(
     GET_USER_PROFILE,
     {
@@ -37,18 +48,46 @@ export default async function Profile({ params }) {
   );
   const user = { ...userMeta, ...userProfile };
 
+  // if user is a club, display the club's logo as profile picture
+  let club = null;
+  if (user.role === "club") {
+    const { data: { club: targetClub } = {} } = await getClient().query(
+      GET_CLUB,
+      { clubInput: { cid: user.uid } }
+    );
+    club = targetClub;
+  }
+
   return (
     <Container>
+      {/* 
+        show action palette only
+        1. if current user is CC, or
+        2. if current user is viewing their own profile and is not a club
+      */}
+      {currentUser.role === "cc" ||
+      (currentUser.uid === user.uid && user.role !== "club") ? (
+        <ActionPalette right={[EditUser]} />
+      ) : null}
       <Grid container spacing={2} mt={4}>
         <Grid item xs={12}>
           <Stack direction="row" alignItems="center" spacing={4}>
-            <UserImage
-              image={user.img}
-              name={user.firstName}
-              gender={user.gender}
-              width={150}
-              height={150}
-            />
+            {club ? (
+              <ClubLogo
+                name={club.name}
+                logo={club.logo}
+                width={150}
+                height={150}
+              />
+            ) : (
+              <UserImage
+                image={user.img}
+                name={user.firstName}
+                gender={user.gender}
+                width={150}
+                height={150}
+              />
+            )}
             <Stack direction="column" spacing={1}>
               <Typography variant="h2" wordWrap="break-word">
                 {user.firstName} {user.lastName}
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,24 +1,7 @@
+import { redirect } from "next/navigation";
+
 import { getClient } from "gql/client";
-import { GET_CLUB } from "gql/queries/clubs";
 import { GET_USER } from "gql/queries/auth";
-
-import { Container, Grid, Stack, Typography } from "@mui/material";
-
-import ClubLogo from "components/clubs/ClubLogo";
-import UserImage from "components/users/UserImage";
-import UserDetails from "components/profile/UserDetails";
-
-export async function generateMetadata() {
-  const { data: { userMeta, userProfile } = {} } = await getClient().query(
-    GET_USER,
-    { userInput: null }
-  );
-  const user = { ...userMeta, ...userProfile };
-
-  return {
-    title: `${user.firstName} ${user.lastName}`,
-  };
-}
 
 export default async function Profile() {
   const { data: { userMeta, userProfile } = {} } = await getClient().query(
@@ -27,58 +10,6 @@ export default async function Profile() {
   );
   const user = { ...userMeta, ...userProfile };
 
-  // if user is a club, display the club's logo as profile picture
-  let club = null;
-  if (user.role === "club") {
-    const { data: { club: targetClub } = {} } = await getClient().query(
-      GET_CLUB,
-      {
-        clubInput: { cid: user.uid },
-      }
-    );
-    club = targetClub;
-  }
-
-  return (
-    <Container>
-      <Grid container spacing={2} mt={4}>
-        <Grid item xs={12}>
-          <Stack direction="row" alignItems="center" spacing={4}>
-            {club ? (
-              <ClubLogo
-                name={club.name}
-                logo={club.logo}
-                width={150}
-                height={150}
-              />
-            ) : (
-              <UserImage
-                image={user.img}
-                name={user.firstName}
-                gender={user.gender}
-                width={150}
-                height={150}
-              />
-            )}
-            <Stack direction="column" spacing={1}>
-              <Typography variant="h2" wordWrap="break-word">
-                {user.firstName} {user.lastName}
-              </Typography>
-              <Typography
-                variant="body1"
-                color="text.secondary"
-                fontFamily="monospace"
-              >
-                {user.email}
-              </Typography>
-            </Stack>
-          </Stack>
-        </Grid>
-
-        <Grid item container xs spacing={2} mt={5}>
-          <UserDetails user={user} />
-        </Grid>
-      </Grid>
-    </Container>
-  );
+  // redirect to user's profile page
+  redirect(`/profile/${user.uid}`);
 }
