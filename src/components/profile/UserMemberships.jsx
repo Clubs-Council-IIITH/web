c82e6c64da5a0b3d684959d822636a0c9e68b1f3
Bhav Beri <bhavberi@gmail.com>
2023-08-10 17:14:51 +0530

    Updated Profile Table pagination options

--- src/components/profile/UserMemberships.jsx
+++ src/components/profile/UserMemberships.jsx
@@ -73,7 +73,9 @@ export default function UserMemberships({ rows = [] }) {
         sorting: {
           sortModel: [{ field: "endYear", sort: "asc" }],
         },
+        pagination: { paginationModel: { pageSize: 10 } },
       }}
+      pageSizeOptions={[5, 10, 25]}
       sx={{
         // disable cell selection style
         ".MuiDataGrid-cell:focus": {
