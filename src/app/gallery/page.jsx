39867693e24f616c5feff4b12d5c92a64be34028
dileepadari <adaridileep@gmail.com>
2023-10-24 11:18:04 +0530

    changes to gallery

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,36 +1,15 @@
 import ImageMasonry from "components/ImageMasonry";
 import ImageModal from "components/ImageModal";
-
-const galleryItems = [
-  "/assets/img/gallery/1.jpg",
-  "/assets/img/gallery/2.jpg",
-  "/assets/img/gallery/3.jpg",
-  "/assets/img/gallery/4.jpg",
-  "/assets/img/gallery/5.jpg",
-  "/assets/img/gallery/6.jpg",
-  "/assets/img/gallery/7.jpg",
-  "/assets/img/gallery/8.jpg",
-  "/assets/img/gallery/9.jpg",
-  "/assets/img/gallery/10.jpg",
-  "/assets/img/gallery/11.jpg",
-  "/assets/img/gallery/12.jpg",
-  "/assets/img/gallery/13.jpg",
-  "/assets/img/gallery/14.jpg",
-  "/assets/img/gallery/15.jpg",
-  "/assets/img/gallery/16.jpg",
-  "/assets/img/gallery/17.jpg",
-  "/assets/img/gallery/18.jpg",
-  "/assets/img/gallery/19.jpg",
-  "/assets/img/gallery/20.jpg",
-  "/assets/img/gallery/21.jpg",
-  "/assets/img/gallery/22.jpg",
-];
+import { getStaticFile } from "utils/files";
 
 export const metadata = {
   title: "Gallery",
 };
 
-export default function Gallery({ searchParams, limit = undefined }) {
+export default async function Gallery({ searchParams, limit = undefined }) {
+  const response = await fetch(getStaticFile('gallery/'));
+  const galleryJSON = await response.json();
+  const galleryItems = galleryJSON.map(item => `${getStaticFile('gallery/')}${item.name}`);
   return (
     <>
       <ImageMasonry
