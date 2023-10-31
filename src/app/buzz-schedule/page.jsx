6e79f87f5ccb0dcab8d7b7275d6592d142e99c50
dileepadari <adaridileep@gmail.com>
2023-10-31 13:15:55 +0530

    changes for buzz-schedule

--- src/app/buzz-schedule/page.jsx
+++ src/app/buzz-schedule/page.jsx
@@ -1,117 +1,213 @@
-import React from "react";
-import { Container, Typography } from "@mui/material";
-import BuzzSchedule from "components/buzz"; 
-import { getStaticFile } from "utils/files";
+"use client";
+import { Typography, Button, Container, Box, Tabs, Tab, Divider } from "@mui/material";
+import { useTheme, alpha } from "@mui/material/styles";
+import { forwardRef } from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import EventsSchedule from "components/buzz/eventsschedule";
 
-export const metadata = {
-  title: "Buzz Schedule",
+const Page = forwardRef(({ children, title = "", meta, ...other }, ref) => (
+  <>
+    <Head>
+      <title>{`${title}`}</title>
+      {meta}
+    </Head>
+
+    <Box ref={ref} {...other}>
+      {children}
+    </Box>
+  </>
+));
+
+Page.displayName = "Page";
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string,
+  meta: PropTypes.node,
 };
 
-export default async function Managebuzz() {
-    // const event = await fetch(getStaticFile('json/buzz.json'));
-    // const events = await event.json();
-  const events = [
+export default async function Induction() {
+  const theme = useTheme();
+  const eventsschedule = [
     {
+      "className": "date"
+    },
+    {
+
+        "className":"red",
         "eventName": "Hacking Club",
         "clubid": "hacking.club",
         "date": "02/11/23 - 04/11/23",
         "time": " - ",
-        "venue": "Online"
+        "location": "Online",
+        "icon":"TerminalOutlined"
     },
     {
+        "className":"blue",
         "eventName": "Gaming Club",
         "clubid": "thegamingclub",
         "date": "04/11/23 - Saturday",
         "time": "2:00 PM - 11:59 PM",
-        "venue": "Bakul WareHouse"
+        "location": "Bakul WareHouse",
+        "icon":"VideogameAssetOutlined"
     },
     {
+        "className":"pink",
         "eventName": "Decore",
         "clubid": "decore",
         "date": "04/11/23 - Saturday",
         "time": "2:00 PM - 4:00 PM",
-        "venue": "(H-105/205)"
+        "location": "(H-105/205)",
+        "icon":"panorama"
     },
     {
+        "className":"green",
         "eventName": "Queer Club",
         "clubid": "queer.club",
         "date": "04/11/23 - Saturday",
         "time": "2:00 PM - 4:00 PM",
-        "venue": "(H-105/SH-1/205/krb)"
+        "location": "(H-105/SH-1/205/krb)",
+        "icon":"TransgenderOutlined"
     },
     {
+        "className":"red",
         "eventName": "ARTSOC",
         "clubid": "chessclub",
         "date": "04/11/23 - Saturday",
         "time": "4:00 PM - 6:00 PM",
-        "venue": "Amphitheater"
+        "location": "Amphitheater",
+        "icon":"arttrack"
     },
     {
+        "className":"blue",
         "eventName": "Dance + Comedy",
         "clubid": "thedancecrew",
         "date": "04/11/23 - Saturday",
         "time": "6:00 PM - 11:59 PM",
-        "venue": "Not mentioned"
+        "location": "Not mentioned",
+        "icon":"MicExternalOnOutlined"
     },
     {
+      "className": "date"
+    },
+    {
+        "className":"green",
         "eventName": "Programming Club",
         "clubid": "programming.club",
         "date": "05/11/23 - Sunday",
         "time": "11:00 AM to 1:00 PM",
-        "venue": "Online"
+        "location": "Online",
+        "icon":"Computer"
     },
     {
+        "className":"pink",
         "eventName": "ASEC",
         "clubid": "sportsclub",
         "date": "05/11/23 - Sunday",
         "time": "11:00 AM to 1:00 PM",
-        "venue": "Football Ground"
+        "location": "Football Ground",
+        "icon":"SportsSoccerOutlined"
     },
     {
+        "className":"blue",
         "eventName": "Fashion Club",
         "clubid": "Fashion Club",
         "date": "05/11/23 - Sunday",
         "time": "2:00 PM to 4:00 PM",
-        "venue": "Not mentioned"
+        "location": "Not mentioned",
+        "icon":""
     },
     {
+        "className":"green",
         "eventName": "Chess Club",
         "clubid": "chessclub",
         "date": "05/11/23 - Sunday",
         "time": "2:00 PM to 6:00 PM",
-        "venue": "D-101"
+        "location": "D-101",
+        "icon":""
     },
     {
+        "className":"red",
         "eventName": "Pentaprism",
         "clubid": "photography",
         "date": "05/11/23 - Sunday",
         "time": "3:00 PM to 6:00 PM",
-        "venue": "Not mentioned"
+        "location": "Not mentioned",
+        "icon":"CameraEnhanceOutlined"
     },
     {
+        "className":"green",
         "eventName": "College Event",
         "clubid": "campuslife",
         "date": "05/11/23 - Sunday",
         "time": "3:00 PM to 6:00 PM",
-        "venue": "H-105"
+        "location": "H-105",
+        "icon":"CampaignOutlined"
     },
     {
+        "className":"pink",
         "eventName": "Music + Dj",
         "clubid": "campuslife",
         "date": "05/11/23 - Sunday",
         "time": "6:00 PM to 11:59 PM",
-        "venue": "Not mentioned"
-    }
+        "location": "Not mentioned",
+        "icon":"NightlifeOutlined"
+    },
+    {
+      "className": "date"
+    },
+    {
+      "className": "empty"
+    },
+    
   ];
-  const eventsWithIds = events.map((event, index) => ({
-    ...event,
-    id: index + 1 } ));
   return (
-    <Container>
-      <Typography variant="h3" align="center" gutterBottom>
-        Manage Clubs & Student Bodies
-      </Typography>
-      <BuzzSchedule events={eventsWithIds}/>
-    </Container>
+    <Page title="Felicity Buzz">
+      <Container>
+        <Typography variant="h3" gutterBottom align="center">
+          Felicity Buzz
+        </Typography>
+        <Typography variant="body1" gutterBottom mt={4} align="center">
+          Organised by <i>Clubs Council</i> & <i>Felicity</i>, in partnership with Clubs on Campus
+        </Typography>
+
+        <Box sx={{ mt: 2 }}>
+          <Tabs
+            value={0}
+            scrollButtons="auto"
+            textColor="black"
+            variant="scrollable"
+            TabIndicatorProps={{
+              style: {
+                backgroundColor: theme.palette.accent,
+              },
+            }}
+            sx={{ backgroundColor: alpha(theme.palette.accent, 0.07) }}
+            fullWidth
+          >
+            <Tab
+              label="Events Schedule"
+              sx={{
+                fontWeight: 600,
+                fontSize: "1.0em",
+                minWidth: "100%",
+                width: "100%",
+              }}
+              wrapped
+            />
+            
+          </Tabs>
+        </Box>
+            <Box sx={{ mt: 3 }}>
+              {
+                // environment && environment === 'production' ?
+                //   <Typography variant="body1"> To be announced soon !<br />Stay Tuned !!</Typography>
+                //   :
+                  !eventsschedule ? <Typography variant="body1">Loading...</Typography>
+                    : <EventsSchedule eventsschedule={eventsschedule} />
+              }
+            </Box>
+      </Container>
+    </Page>
   );
 }
---
+++ src/components/buzz/eventsschedule.jsx
@@ -0,0 +1,215 @@
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import {
+    RemoveOutlined,
+    TodayOutlined,
+    Groups2Outlined,
+    MeetingRoomOutlined,
+    MicExternalOnOutlined,
+    EmojiEventsOutlined,
+    TransgenderOutlined,
+    PrecisionManufacturing,
+    PlayCircleFilledOutlined,
+    Computer,
+    TravelExploreOutlined,
+    NightlifeOutlined,
+    BrushOutlined,
+    LanguageOutlined,
+    MovieCreationOutlined,
+    CampaignOutlined,
+    PsychologyOutlined,
+    Woman2Outlined,
+    SetMealOutlined,
+    SchoolOutlined,
+    ColorLensOutlined,
+    NoFoodOutlined,
+    PeopleOutlined,
+    TheaterComedyOutlined,
+    LuggageOutlined,
+    CameraEnhanceOutlined,
+    ModeOfTravelOutlined,
+    QuizOutlined,
+    BugReportOutlined,
+    CleaningServicesOutlined,
+    EmojiEmotionsOutlined,
+    TableRowsOutlined,
+    SkateboardingOutlined,
+    NightsStayOutlined,
+    AbcOutlined,
+    SportsSoccerOutlined,
+    EmojiSymbolsOutlined,
+    AgricultureOutlined,
+    VideogameAssetOutlined,
+    PetsOutlined,
+    BorderColorOutlined,
+    TerminalOutlined,
+    CodeOutlined,
+    ArtTrackOutlined,
+    Panorama,
+} from "@mui/icons-material";
+import { Divider } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+const IconsList = {
+    "PetsOutlined": <PetsOutlined />,
+    "arttrack": <ArtTrackOutlined/>,
+    "panorama":<Panorama/>,
+    "Groups2Outlined": <Groups2Outlined />,
+    "MovieCreationOutlined": <MovieCreationOutlined />,
+    "MeetingRoomOutlined": <MeetingRoomOutlined />,
+    "MicExternalOnOutlined": <MicExternalOnOutlined />,
+    "EmojiEventsOutlined": <EmojiEventsOutlined />,
+    "TransgenderOutlined": <TransgenderOutlined />,
+    "CampaignOutlined": <CampaignOutlined />,
+    "PsychologyOutlined": <PsychologyOutlined />,
+    "Woman2Outlined": <Woman2Outlined />,
+    "SetMealOutlined": <SetMealOutlined />,
+    "PrecisionManufacturing": <PrecisionManufacturing />,
+    "PlayCircleFilledOutlined": <PlayCircleFilledOutlined />,
+    "Computer": <Computer />,
+    "TravelExploreOutlined": <TravelExploreOutlined />,
+    "NightlifeOutlined": <NightlifeOutlined />,
+    "BrushOutlined": <BrushOutlined />,
+    "LanguageOutlined": <LanguageOutlined />,
+    "SchoolOutlined": <SchoolOutlined />,
+    "ColorLensOutlined": <ColorLensOutlined />,
+    "NoFoodOutlined": <NoFoodOutlined />,
+    "PeopleOutlined": <PeopleOutlined />,
+    "ModeOfTravelOutlined": <ModeOfTravelOutlined />,
+    "TheaterComedyOutlined": <TheaterComedyOutlined />,
+    "LuggageOutlined": <LuggageOutlined />,
+    "CameraEnhanceOutlined": <CameraEnhanceOutlined />,
+    "BugReportOutlined": <BugReportOutlined />,
+    "CleaningServicesOutlined": <CleaningServicesOutlined />,
+    "TableRowsOutlined": <TableRowsOutlined />,
+    "SkateboardingOutlined": <SkateboardingOutlined />,
+    "NightsStayOutlined": <NightsStayOutlined />,
+    "EmojiEmotionsOutlined": <EmojiEmotionsOutlined />,
+    "AbcOutlined": <AbcOutlined />,
+    "SportsSoccerOutlined": <SportsSoccerOutlined />,
+    "QuizOutlined": <QuizOutlined />,
+    "EmojiSymbolsOutlined": <EmojiSymbolsOutlined />,
+    "AgricultureOutlined": <AgricultureOutlined />,
+    "BorderColorOutlined": <BorderColorOutlined />,
+    "PetsOutlined": <PetsOutlined />,
+    "TerminalOutlined": <TerminalOutlined />,
+    "CodeOutlined": <CodeOutlined />,
+    "VideogameAssetOutlined": <VideogameAssetOutlined />,
+};
+function getRandomColor() {
+    return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+  }
+
+const TimelineElement = ({
+    eventName,
+    clubid,
+    location,
+    time,
+    description,
+    className = "others",
+    date,
+    icon: Icon = <></>,
+    theme = null
+}) => (
+    className === "empty" ? (
+        <VerticalTimelineElement
+            iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+            icon={<RemoveOutlined />}
+        />
+    ) : className === "date" ? (
+        <VerticalTimelineElement
+            iconStyle={{ background: "rgb(0,0,0)", color: "#fff" }}
+            icon={<TodayOutlined />}
+        />
+    ) : className === "blue" ? (
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(18, 110, 184)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgb(18, 110, 184)" }}
+            date={date}
+            dateClassName="timelineDate"
+            iconStyle={{ background: "rgb(18, 110, 184)", color: "#fff" }}
+            icon={Icon}
+        >
+            <h3 className="vertical-timeline-element-title"><u>{eventName}</u></h3>
+            <h5>{time}</h5>
+            <h5 className="vertical-timeline-element-subtitle">{location}</h5>
+            <p>{description}</p>
+        </VerticalTimelineElement>
+    ) : className === "red" ? (
+        <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            contentStyle={{ background: "rgb(233, 30, 90)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgb(233, 30, 90)" }}
+            date={date}
+            dateClassName="timelineDate"
+            iconStyle={{ background: "rgb(233, 30, 90)", color: "#fff" }}
+            icon={Icon}
+        >
+            <h3 className="vertical-timeline-element-title"><u>{eventName}</u></h3>
+            <h5>{time}</h5>
+            <h5 className="vertical-timeline-element-subtitle">{location}</h5>
+            <p>{description}</p>
+        </VerticalTimelineElement>
+    ) : className === "yellow" ? (
+        <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            contentStyle={{ background: "rgb(168, 157, 5)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgb(168, 157, 5)" }}
+            date={date}
+            dateClassName="timelineDate"
+            iconStyle={{ background: "rgb(168, 157, 5)", color: "#fff" }}
+            icon={Icon}
+        >
+            <h3 className="vertical-timeline-element-title"><u>{eventName}</u></h3>
+            <h5>{time}</h5>
+            <h5 className="vertical-timeline-element-subtitle">{location}</h5>
+            <p>{description}</p>
+        </VerticalTimelineElement>
+    ) : className === "green" ? (
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgb(16, 204, 82)" }}
+            date={date}
+            dateClassName="timelineDate"
+            iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+            icon={Icon}
+        >
+            <h3 className="vertical-timeline-element-title"><u>{eventName}</u></h3>
+            <h5>{time}</h5>
+            <h5 className="vertical-timeline-element-subtitle">{location}</h5>
+            <p>{description}</p>
+        </VerticalTimelineElement>
+    ) : (
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(176, 9, 127)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid rgb(176, 9, 127)" }}
+            date={date}
+            dateClassName="timelineDate"
+            iconStyle={{ background: "rgb(176, 9, 127)", color: "#fff" }}
+            icon={Icon}
+        >
+            <h3 className="vertical-timeline-element-title"><u>{eventName}</u></h3>
+            <h5>{time}</h5>
+            <h5 className="vertical-timeline-element-subtitle">{location}</h5>
+            <p>{description}</p>
+        </VerticalTimelineElement>
+    )
+);
+
+export default function EventsSchedule({eventsschedule}) {
+    const theme = useTheme();
+    return (
+        <>
+            <VerticalTimeline lineColor="black">
+                {eventsschedule.map((member, key) => (
+                    <TimelineElement {...member} icon={IconsList[member["icon"]]} theme={theme} key={key}></TimelineElement>
+                ))}
+            </VerticalTimeline>
+            <Divider />
+        </>
+    );
+}
