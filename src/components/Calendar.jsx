50cd38b668a146df561dc246c5b2a367f096df78
Bhav Beri <bhavberi@gmail.com>
2024-01-06 15:47:23 +0530

    Updated Calendar to show other clubs events (with click disbaled)

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,25 +4,56 @@ import stc from "string-to-color";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
-function eventDataTransform(event) {
-  return {
-    id: event._id,
-    title: event.name,
-    start: new Date(event.datetimeperiod[0]),
-    end: new Date(event.datetimeperiod[1]),
-    backgroundColor: stc(event.clubid),
-    url: `/events/${event._id}`,
-    display: "block",
-  };
+import { useAuth } from "components/AuthProvider";
+
+function eventDataTransform(event, role, uid) {
+  if (event.status.state === "approved") {
+    return {
+      id: event._id,
+      title: event.name,
+      start: new Date(event.datetimeperiod[0]),
+      end: new Date(event.datetimeperiod[1]),
+      backgroundColor: stc(event.clubid),
+      url: `/events/${event._id}`,
+      display: "block",
+    };
+  }
+  else {
+    if (role == "cc" ||  uid == event.clubid)
+      return {
+        id: event._id,
+        title: event.name,
+        start: new Date(event.datetimeperiod[0]),
+        end: new Date(event.datetimeperiod[1]),
+        backgroundColor: stc(event.clubid),
+        url: `/manage/events/${event._id}`,
+        display: "block",
+      };
+    else
+      return {
+        id: event._id,
+        title: event.name,
+        start: new Date(event.datetimeperiod[0]),
+        end: new Date(event.datetimeperiod[1]),
+        backgroundColor: stc(event.clubid),
+        display: "block",
+      };
+  }
 }
 
 export default function Calendar({ events }) {
+  const { user } = useAuth();
+
+  const eventDataTransform_withrole = (event) => {
+    return eventDataTransform(event, user?.role, user?.uid);
+  };
+
   return (
     <FullCalendar
       events={events}
       plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
-      eventDataTransform={eventDataTransform}
+      eventDataTransform={eventDataTransform_withrole}
     />
   );
 }
