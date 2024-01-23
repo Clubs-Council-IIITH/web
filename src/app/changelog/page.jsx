51f1fccca35134c43fa08660a5279dd9997f904a
Bhav Beri <bhavberi@gmail.com>
2024-01-23 12:54:56 +0530

    Changelog show the first 20 entries, and give an option to see more

--- src/app/changelog/page.jsx
+++ src/app/changelog/page.jsx
@@ -1,17 +1,23 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 
-import { Container, Typography } from "@mui/material";
+import { Container, Box, Typography, Stack, Button } from "@mui/material";
+import Link from "next/link";
+import Icon from "components/Icon";
 
 import Status from "./status.mdx";
 
 import LocalUsersGrid from "components/users/LocalUsersGrid";
 import { getStaticFile } from "utils/files";
 
+const limit = 20;
+
 export const metadata = {
   title: "Changelog",
 };
 
-export default async function Changelog() {
+export default async function Changelog({ searchParams }) {
+  const show_all = searchParams?.all === "true" ? true : false;
+
   const status = await fetch(getStaticFile("json/status.json"), {
     cache: "no-store",
   });
@@ -21,6 +27,8 @@ export default async function Changelog() {
   const techMembers = await fetch(getStaticFile("json/techMembers.json"), {
     next: { revalidate: 60 },
   });
+
+  let logsText = await logs.text();
 
   return (
     <Container>
@@ -32,10 +40,49 @@ export default async function Changelog() {
       </Typography>
       <LocalUsersGrid users={await techMembers.json()} />
 
-      <Typography variant="h3" mt={3}>
-        Changelog
-      </Typography>
-      <MDXRemote source={await logs.text()} />
+      <Stack direction="row" pt={2} mb={2} mt={3}>
+        <Typography variant="h3">Changelog</Typography>
+        {logsText.split("\n").length > limit ? <>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box display="flex" alignItems="center">
+            {show_all ? (
+              <Button
+                variant="none"
+                color="secondary"
+                component={Link}
+                href="/changelog"
+              >
+                <Icon variant="chevron-left" />
+                <Typography variant="button" color="text.primary">
+                  View less
+                </Typography>
+              </Button>
+            ) : (
+              <Button
+                variant="none"
+                color="secondary"
+                component={Link}
+                href="/changelog?all=true"
+              >
+                <Typography variant="button" color="text.primary">
+                  View all
+                </Typography>
+                <Icon variant="chevron-right" />
+              </Button>
+            )
+            }
+          </Box>
+        </> : null}
+      </Stack>
+
+      <MDXRemote source={show_all ? logsText : logsText?.split("\n").slice(0, limit).join("\n")} />
+
+      {logsText.split("\n").length > limit ? (
+        <Typography variant="body2" color="text.secondary" mb={2}>
+          Showing {show_all ? logsText?.split("\n").length : limit} of {logsText?.split("\n").length} entries.
+        </Typography>
+      ) : null}
+
     </Container>
   );
 }
