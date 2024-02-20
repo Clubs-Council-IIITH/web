0529e77101ef2ae37c93e3e04b3983e47250452f
Bhav Beri <bhavberi@gmail.com>
2024-02-20 13:54:50 +0530

    Updated the events by [code] page rendering, as in new nextjs versions redirect returns NEXT_REDIRECT error

--- src/app/manage/events/code/[code]/page.jsx
+++ src/app/manage/events/code/[code]/page.jsx
@@ -6,14 +6,12 @@ import { GET_EVENT_ID_FROM_CODE } from "gql/queries/events";
 export default async function EventByCode({ params }) {
   const { code } = params;
 
-  try {
-    const { data: { eventid } = {} } = await getClient().query(
-      GET_EVENT_ID_FROM_CODE,
-      { code },
-    );
-  } catch (error) {
+  const { data = {}, error } = await getClient().query(
+    GET_EVENT_ID_FROM_CODE,
+    { code },
+  );
+  if (error || !data?.eventid)
     redirect("/404");
-  }
 
-  return redirect(`/manage/events/${eventid}`);
+  redirect(`/manage/events/${data.eventid}`);
 }
