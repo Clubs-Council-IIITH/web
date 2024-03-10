12a30fbb68aefb205d4c2f0587794eb3fc7f199d
Bhav Beri <bhavberi@gmail.com>
2024-03-10 12:39:03 +0530

    Setup logout callback

--- src/utils/auth.jsx
+++ src/utils/auth.jsx
@@ -10,12 +10,17 @@ export function login(pathname) {
   window.location.replace("/login");
 }
 
-export function logout(pathname) {
+export async function logout(pathname) {
   // save path to continue from
   setCookie("continue", pathname);
 
   // set flag to expire token the next time someone visits the site, because CAS doesn't follow ?service for some reason
-  setCookie("logout", true);
+  // setCookie("logout", true);
+
+  // console.log("Logging out...");
+
+  // fetch logout page to expire token
+  await fetch("/logoutCallback");
 
   // redirect to CAS logout
   window.location.replace("/logout");
