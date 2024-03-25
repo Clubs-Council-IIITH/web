a10b9509d85169a9bc412ca0b5cfbe24ad033982
Bhav Beri <bhavberi@gmail.com>
2024-03-25 17:11:09 +0530

    Already applied user can't apply again

--- src/app/cc-recruitments/page.jsx
+++ src/app/cc-recruitments/page.jsx
@@ -19,19 +19,17 @@ export const metadata = {
 
 const deadline = new Date("2024-04-10T11:59:00Z");
 
-async function getUser(currentUser){
-      // get target user
-      console.log('currentUser:', currentUser)
-      const { data: { userProfile, userMeta } = {} } = await getClient().query(
-        GET_USER_PROFILE,
-        {
-          userInput: {
-            uid: currentUser.uid,
-          },
-        },
-      );
-      const user = { ...userMeta, ...userProfile };
-      return user;
+async function getUser(currentUser) {
+  const { data: { userProfile, userMeta } = {} } = await getClient().query(
+    GET_USER_PROFILE,
+    {
+      userInput: {
+        uid: currentUser.uid,
+      },
+    }
+  );
+  const user = { ...userMeta, ...userProfile };
+  return user;
 }
 
 export default async function NewApplication() {
@@ -52,23 +50,21 @@ export default async function NewApplication() {
         <Typography variant="h4" gutterBottom mt={3} align="center">
           The form is disabled
         </Typography>
+      ) : haveAppliedForCC ? (
+        <>
+          <Typography variant="h5" gutterBottom mt={6} align="center">
+            You have already applied for Clubs Council Position. Thank you!
+          </Typography>
+          <Typography variant="h6" gutterBottom mt={3} align="center">
+            You will be notified about the further stages soon.
+          </Typography>
+        </>
       ) : (
-        // ) : (
-        // haveAppliedForCC ? (
-        //   <>
-        //     <Typography variant="h5" gutterBottom mt={6} align="center">
-        //       You have already applied for Clubs Council Position. Thank you!
-        //     </Typography>
-        //     <Typography variant="h6" gutterBottom mt={3} align="center">
-        //       You will be notified about the further stages soon.
-        //     </Typography>
-        //   </>
-        // ) : (
         <>
           <Typography variant="h3" gutterBottom mb={3} align="center">
             Apply for Clubs Council
           </Typography>
-{/* 
+
           <Typography variant="h5" gutterBottom mb={3}>
             Instructions
           </Typography>
@@ -76,20 +72,20 @@ export default async function NewApplication() {
           <Typography variant="body1" gutterBottom mb={3}>
             Add deadline for the form submission as some date, after which the
             form will be disabled - and show a message that the form is disabled
-          </Typography> */}
+          </Typography>
 
           <Typography variant="h5" gutterBottom mb={3}>
             Application Form
           </Typography>
 
-          <RecruitmentForm user={user}/>
+          <RecruitmentForm user={user} />
         </>
       )}
 
-      {/* <Typography variant="body2" gutterBottom mt={10} align="center">
+      <Typography variant="body2" gutterBottom mt={10} align="center">
         Made with ❤️ by the SLC Tech Team. For any queries contact us at{" "}
         <a href="mailto:clubs@iiit.ac.in"> clubs@iiit.ac.in </a>
-      </Typography> */}
+      </Typography>
     </Container>
   );
 }
