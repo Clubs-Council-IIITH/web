a0e0678865ae735a5ab66cb0a89f182ad83e031b
The-Broken-Keyboard <saurabhshreyansh868@gmail.com>
2024-07-03 11:54:49 +0530

    fixed window not defined issue

--- src/contexts/ModeContext.jsx
+++ src/contexts/ModeContext.jsx
@@ -1,6 +1,6 @@
 // ModeContext.js
 "use client";
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
 const ModeContext = createContext({
@@ -11,21 +11,30 @@ const ModeContext = createContext({
 export const useMode = () => useContext(ModeContext);
 
 export const ModeProvider = ({ children }) => {
-  let theme_bool;
-  if (window.localStorage.getItem("currentModeCC") === "false") {
-    theme_bool = false;
-  } else if (window.localStorage.getItem("currentModeCC") === "true") {
-    theme_bool = true;
-  } else {
-    theme_bool = useMediaQuery("(prefers-color-scheme: dark)");
-    console.log("theme_bool", theme_bool);
-  }
+  const [isDark, setIsDark] = useState(true);
 
-  const [isDark, setisDark] = useState(theme_bool);
+  useEffect(() => {
+    let theme_bool;
+    if (typeof window !== "undefined") {
+      const storedMode = window.localStorage.getItem("currentModeCC");
+      if (storedMode === "false") {
+        theme_bool = false;
+      } else if (storedMode === "true") {
+        theme_bool = true;
+      } else {
+        theme_bool = useMediaQuery("(prefers-color-scheme: dark)");
+        console.log("theme_bool", theme_bool);
+      }
+
+      setIsDark(theme_bool);
+    }
+  }, []);
 
   const setMode = (mode) => {
-    setisDark(mode);
-    window.localStorage.setItem("currentModeCC", mode);
+    setIsDark(mode);
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem("currentModeCC", mode);
+    }
   };
 
   return (
