bb810754d5c779f79f03da28e8cccfc6a2140176
Bhav Beri <bhavberi@gmail.com>
2024-07-03 13:35:54 +0530

    Mode show loading while fetching

--- src/contexts/ModeContext.jsx
+++ src/contexts/ModeContext.jsx
@@ -1,6 +1,7 @@
 // ModeContext.js
 "use client";
 import React, { createContext, useState, useContext, useEffect } from "react";
+import { CircularProgress } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
 const ModeContext = createContext({
@@ -8,12 +9,32 @@ const ModeContext = createContext({
   setMode: () => {},
 });
 
+function GradientCircularProgress() {
+  return (
+    <React.Fragment>
+      <svg width={0} height={0}>
+        <defs>
+          <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" stopColor="#e01cd5" />
+            <stop offset="100%" stopColor="#1CB5E0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <CircularProgress sx={{ 'svg circle': { stroke: 'url(#my_gradient)' } }} />
+    </React.Fragment>
+  );
+}
+
+
+
 export const useMode = () => useContext(ModeContext);
 
 export const ModeProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(false);
-  let theme_bool;
-  theme_bool = useMediaQuery("(prefers-color-scheme: dark)");
+  const [isLoading, setIsLoading] = useState(true);
+
+  let theme_bool = useMediaQuery("(prefers-color-scheme: dark)");
+
   useEffect(() => {
     if (typeof window !== "undefined") {
       const storedMode = window.localStorage.getItem("currentModeCC");
@@ -21,8 +42,9 @@ export const ModeProvider = ({ children }) => {
         theme_bool = false;
       } else if (storedMode === "true") {
         theme_bool = true;
-      } 
+      }
       setIsDark(theme_bool);
+      setIsLoading(false);
     }
   }, [theme_bool]);
 
@@ -35,7 +57,20 @@ export const ModeProvider = ({ children }) => {
 
   return (
     <ModeContext.Provider value={{ isDark, setMode }}>
-      {children}
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <GradientCircularProgress />
+        </div>
+      ) : (
+        children
+      )}
     </ModeContext.Provider>
   );
 };
