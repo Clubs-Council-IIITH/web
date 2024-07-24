6ca608ce41bbdddfc8e4e9773f3e876cdb6d73ca
Dileepadari <Dileepadari@users.noreply.github.com>
2024-07-24 20:02:30 +0000

    Apply Prettier Formatting Fixes

--- src/components/profile/AccountPopover.jsx
+++ src/components/profile/AccountPopover.jsx
@@ -26,7 +26,7 @@ export default function AccountPopover() {
   const pathname = usePathname();
   const { user, isAuthenticated } = useAuth();
   const { isDark, setMode } = useMode(); // Accessing isDark and setMode from ModeContext
-  const {isiframe, setIsiframe}=useMode2();
+  const { isiframe, setIsiframe } = useMode2();
   const [open, setOpen] = React.useState(null);
 
   const handleChange = () => {
@@ -116,54 +116,56 @@ export default function AccountPopover() {
           ) : null}
         </Avatar>
       </IconButton>
-      
-      {!isiframe && (<Popover
-        open={Boolean(open)}
-        anchorEl={open}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        PaperProps={{
-          sx: {
-            p: 0,
-            mt: 1.5,
-            ml: 0.75,
-            width: 180,
-            "& .MuiMenuItem-root": {
-              typography: "body2",
-              borderRadius: 0.75,
+
+      {!isiframe && (
+        <Popover
+          open={Boolean(open)}
+          anchorEl={open}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          transformOrigin={{ vertical: "top", horizontal: "right" }}
+          PaperProps={{
+            sx: {
+              p: 0,
+              mt: 1.5,
+              ml: 0.75,
+              width: 180,
+              "& .MuiMenuItem-root": {
+                typography: "body2",
+                borderRadius: 0.75,
+              },
             },
-          },
-        }}
-      >
-      
-        <ModeSwitch checked={isDark} onChange={handleChange} sx={{ m: 1 }} />
-      
-        {/* <ModeSwitch checked={isDark} onChange={handleChange} sx={{ m: 1 }} />{" "} */}
-        {/* Pass current isDark value and handleChange function to ModeSwitch component */}
-        {isAuthenticated ? (
-          <>
-            <Stack sx={{ my: 1.5, px: 2.5 }}>
-              <Typography variant="subtitle2" noWrap>
-                {`${user?.firstName} ${user?.lastName}`}
-              </Typography>
-              <Typography
-                variant="body2"
-                sx={{ color: "text.secondary" }}
-                noWrap
-              >
-                {user?.email}
-              </Typography>
-            </Stack>
+          }}
+        >
+          <ModeSwitch checked={isDark} onChange={handleChange} sx={{ m: 1 }} />
 
-            {[...AUTHENTICATED_MENU_OPTIONS, ...COMMON_MENU_OPTIONS].length >
-            0 ? (
-              <>
-                <Divider sx={{ borderStyle: "dashed" }} />
+          {/* <ModeSwitch checked={isDark} onChange={handleChange} sx={{ m: 1 }} />{" "} */}
+          {/* Pass current isDark value and handleChange function to ModeSwitch component */}
+          {isAuthenticated ? (
+            <>
+              <Stack sx={{ my: 1.5, px: 2.5 }}>
+                <Typography variant="subtitle2" noWrap>
+                  {`${user?.firstName} ${user?.lastName}`}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "text.secondary" }}
+                  noWrap
+                >
+                  {user?.email}
+                </Typography>
+              </Stack>
 
-                <Stack sx={{ p: 1 }}>
-                  {[...AUTHENTICATED_MENU_OPTIONS, ...COMMON_MENU_OPTIONS].map(
-                    (option) => (
+              {[...AUTHENTICATED_MENU_OPTIONS, ...COMMON_MENU_OPTIONS].length >
+              0 ? (
+                <>
+                  <Divider sx={{ borderStyle: "dashed" }} />
+
+                  <Stack sx={{ p: 1 }}>
+                    {[
+                      ...AUTHENTICATED_MENU_OPTIONS,
+                      ...COMMON_MENU_OPTIONS,
+                    ].map((option) => (
                       <MenuItem
                         component={Link}
                         key={option.label}
@@ -172,45 +174,45 @@ export default function AccountPopover() {
                         <Icon variant={option.icon} sx={{ mr: 2 }} />
                         {option.label}
                       </MenuItem>
-                    ),
-                  )}
-                </Stack>
-              </>
-            ) : null}
+                    ))}
+                  </Stack>
+                </>
+              ) : null}
 
-            <Divider sx={{ borderStyle: "dashed" }} />
+              <Divider sx={{ borderStyle: "dashed" }} />
 
-            <MenuItem onClick={() => logout(pathname)} sx={{ m: 1 }}>
-              Logout
-            </MenuItem>
-          </>
-        ) : (
-          <>
-            {COMMON_MENU_OPTIONS.length > 0 ? (
-              <>
-                <Stack sx={{ p: 1 }}>
-                  {COMMON_MENU_OPTIONS.map((option) => (
-                    <MenuItem
-                      component={Link}
-                      key={option.label}
-                      href={option.url}
-                    >
-                      <Icon variant={option.icon} sx={{ mr: 2 }} />
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </Stack>
+              <MenuItem onClick={() => logout(pathname)} sx={{ m: 1 }}>
+                Logout
+              </MenuItem>
+            </>
+          ) : (
+            <>
+              {COMMON_MENU_OPTIONS.length > 0 ? (
+                <>
+                  <Stack sx={{ p: 1 }}>
+                    {COMMON_MENU_OPTIONS.map((option) => (
+                      <MenuItem
+                        component={Link}
+                        key={option.label}
+                        href={option.url}
+                      >
+                        <Icon variant={option.icon} sx={{ mr: 2 }} />
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </Stack>
 
-                <Divider sx={{ borderStyle: "dashed" }} />
-              </>
-            ) : null}
+                  <Divider sx={{ borderStyle: "dashed" }} />
+                </>
+              ) : null}
 
-            <MenuItem onClick={() => login(pathname)} sx={{ m: 1 }}>
-              Login
-            </MenuItem>
-          </>
-        )}
-        </Popover>)}
+              <MenuItem onClick={() => login(pathname)} sx={{ m: 1 }}>
+                Login
+              </MenuItem>
+            </>
+          )}
+        </Popover>
+      )}
     </>
   );
 }
