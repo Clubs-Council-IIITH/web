bfd826a3e4de1be6be8d0f38891da5b65b88fd69
bhavberi <bhavberi@users.noreply.github.com>
2024-07-25 03:55:25 +0000

    Apply Prettier Formatting Fixes

--- src/components/profile/AccountPopover.jsx
+++ src/components/profile/AccountPopover.jsx
@@ -54,69 +54,73 @@ export default function AccountPopover() {
 
   return (
     <>
-      {!isiframe && (<IconButton
-        onClick={handleOpen}
-        sx={{
-          p: 0,
-          color: "white",
-          ...(open && {
-            "&:before": {
-              zIndex: 1,
-              content: "''",
-              width: "100%",
-              height: "100%",
-              borderRadius: "50%",
-              position: "absolute",
-              bgcolor: (theme) => alpha(theme.palette.background.neutral, 0.5),
-            },
-          }),
-          ...(!open && {
-            // "&:after": {
-            "&:before": {
-              zIndex: 1,
-              content: "''",
-              width: "100%",
-              height: "100%",
-              borderRadius: "50%",
-              position: "absolute",
-              bgcolor: (theme) => alpha(theme.palette.background.neutral, 0.1),
-            },
-            // },
-          }),
-        }}
-      >
-        <Avatar
-          width={40}
-          height={40}
-          {...(user?.firstName && {
-            children: `${user?.firstName?.[0]}${
-              user?.lastName == "" ? "" : user?.lastName?.[0]
-            }`,
-          })}
-        >
-          {user?.img ? (
-            <Image
-              alt={user?.firstName}
-              src={getFile(user?.img)}
-              width={400}
-              height={400}
-              style={{
-                top: 0,
-                left: 0,
+      {!isiframe && (
+        <IconButton
+          onClick={handleOpen}
+          sx={{
+            p: 0,
+            color: "white",
+            ...(open && {
+              "&:before": {
+                zIndex: 1,
+                content: "''",
                 width: "100%",
                 height: "100%",
-                objectFit: "cover",
+                borderRadius: "50%",
                 position: "absolute",
-              }}
-            />
-          ) : user?.firstName ? (
-            `${user?.firstName?.[0]}${
-              user?.lastName === "" ? "" : user?.lastName?.[0]
-            }`
-          ) : null}
-        </Avatar>
-      </IconButton>)}
-      
+                bgcolor: (theme) =>
+                  alpha(theme.palette.background.neutral, 0.5),
+              },
+            }),
+            ...(!open && {
+              // "&:after": {
+              "&:before": {
+                zIndex: 1,
+                content: "''",
+                width: "100%",
+                height: "100%",
+                borderRadius: "50%",
+                position: "absolute",
+                bgcolor: (theme) =>
+                  alpha(theme.palette.background.neutral, 0.1),
+              },
+              // },
+            }),
+          }}
+        >
+          <Avatar
+            width={40}
+            height={40}
+            {...(user?.firstName && {
+              children: `${user?.firstName?.[0]}${
+                user?.lastName == "" ? "" : user?.lastName?.[0]
+              }`,
+            })}
+          >
+            {user?.img ? (
+              <Image
+                alt={user?.firstName}
+                src={getFile(user?.img)}
+                width={400}
+                height={400}
+                style={{
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  position: "absolute",
+                }}
+              />
+            ) : user?.firstName ? (
+              `${user?.firstName?.[0]}${
+                user?.lastName === "" ? "" : user?.lastName?.[0]
+              }`
+            ) : null}
+          </Avatar>
+        </IconButton>
+      )}
+
       <Popover
         open={Boolean(open)}
         anchorEl={open}
@@ -136,9 +140,8 @@ export default function AccountPopover() {
           },
         }}
       >
-      
         <ModeSwitch checked={isDark} onChange={handleChange} sx={{ m: 1 }} />
-      
+
         {/* <ModeSwitch checked={isDark} onChange={handleChange} sx={{ m: 1 }} />{" "} */}
         {/* Pass current isDark value and handleChange function to ModeSwitch component */}
         {isAuthenticated ? (
@@ -156,16 +159,14 @@ export default function AccountPopover() {
               </Typography>
             </Stack>
 
-              {[...AUTHENTICATED_MENU_OPTIONS, ...COMMON_MENU_OPTIONS].length >
-              0 ? (
-                <>
-                  <Divider sx={{ borderStyle: "dashed" }} />
+            {[...AUTHENTICATED_MENU_OPTIONS, ...COMMON_MENU_OPTIONS].length >
+            0 ? (
+              <>
+                <Divider sx={{ borderStyle: "dashed" }} />
 
-                  <Stack sx={{ p: 1 }}>
-                    {[
-                      ...AUTHENTICATED_MENU_OPTIONS,
-                      ...COMMON_MENU_OPTIONS,
-                    ].map((option) => (
+                <Stack sx={{ p: 1 }}>
+                  {[...AUTHENTICATED_MENU_OPTIONS, ...COMMON_MENU_OPTIONS].map(
+                    (option) => (
                       <MenuItem
                         component={Link}
                         key={option.label}
@@ -174,44 +175,45 @@ export default function AccountPopover() {
                         <Icon variant={option.icon} sx={{ mr: 2 }} />
                         {option.label}
                       </MenuItem>
-                    ))}
-                  </Stack>
-                </>
-              ) : null}
+                    ),
+                  )}
+                </Stack>
+              </>
+            ) : null}
 
-              <Divider sx={{ borderStyle: "dashed" }} />
+            <Divider sx={{ borderStyle: "dashed" }} />
 
-              <MenuItem onClick={() => logout(pathname)} sx={{ m: 1 }}>
-                Logout
-              </MenuItem>
-            </>
-          ) : (
-            <>
-              {COMMON_MENU_OPTIONS.length > 0 ? (
-                <>
-                  <Stack sx={{ p: 1 }}>
-                    {COMMON_MENU_OPTIONS.map((option) => (
-                      <MenuItem
-                        component={Link}
-                        key={option.label}
-                        href={option.url}
-                      >
-                        <Icon variant={option.icon} sx={{ mr: 2 }} />
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </Stack>
+            <MenuItem onClick={() => logout(pathname)} sx={{ m: 1 }}>
+              Logout
+            </MenuItem>
+          </>
+        ) : (
+          <>
+            {COMMON_MENU_OPTIONS.length > 0 ? (
+              <>
+                <Stack sx={{ p: 1 }}>
+                  {COMMON_MENU_OPTIONS.map((option) => (
+                    <MenuItem
+                      component={Link}
+                      key={option.label}
+                      href={option.url}
+                    >
+                      <Icon variant={option.icon} sx={{ mr: 2 }} />
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </Stack>
 
-                  <Divider sx={{ borderStyle: "dashed" }} />
-                </>
-              ) : null}
+                <Divider sx={{ borderStyle: "dashed" }} />
+              </>
+            ) : null}
 
             <MenuItem onClick={() => login(pathname)} sx={{ m: 1 }}>
               Login
             </MenuItem>
           </>
         )}
-        </Popover>
+      </Popover>
     </>
   );
 }
