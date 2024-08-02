af68acdaf313b45970b90d6c4102b2a63b636e50
bhavberi <bhavberi@users.noreply.github.com>
2024-08-02 06:17:59 +0000

    Apply Prettier Formatting Fixes

--- src/app/manage/events/[id]/page.jsx
+++ src/app/manage/events/[id]/page.jsx
@@ -290,7 +290,7 @@ export default async function ManageEvent({ params }) {
 
   const { data: { userMeta, userProfile } = {} } = await getClient().query(
     GET_USER,
-    { userInput: null }
+    { userInput: null },
   );
   const user = { ...userMeta, ...userProfile };
 
