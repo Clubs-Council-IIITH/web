4a173053d619db83fa330c603c20a83d07f05421
Bhav Beri <bhavberi@gmail.com>
2024-09-05 12:02:17 +0530

    Updated site metadata for better SEO

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,16 +11,52 @@ import { AuthProvider } from "components/AuthProvider";
 import { fontClass } from "components/ThemeRegistry/typography";
 import TransitionProvider from "components/TransitionProvider";
 
+const description =
+  "Discover the vibrant campus life at IIIT Hyderabad through the Clubs Council. Explore diverse student-led clubs, and events that foster an inclusive community and enrich student experiences beyond the classroom. Stay updated on activities, events, and opportunities to engage and grow at IIIT-H.";
+const shortDescription =
+  "Explore with the Clubs Council at IIIT Hyderabad for diverse student-led clubs & events that enrich campus life and foster community.";
+
 export const metadata = {
   title: "Clubs Council IIITH",
-  description: "Clubs Council IIITH", // TODO: change
+  authors: [
+    { name: "Clubs Council" },
+    { name: "IIITH" },
+    { name: "SLC Tech Team", url: "https://github.com/Clubs-Council-IIITH/" },
+  ],
+  publisher: "SLC Tech Team",
+  description: description,
+  generator: "Next.js",
+  keywords: [
+    "IIIT Hyderabad",
+    "Clubs Council",
+    "Student Clubs",
+    "Student Bodies",
+    "Campus Life",
+    "Student Life",
+    "Student Activities",
+    "Events",
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_IN",
+    title: "Clubs Council @ IIIT Hyderabad",
+    description: shortDescription,
+    images: [
+      {
+        url: "https://clubs.iiit.ac.in/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Clubs Council",
+      },
+    ],
+  },
 };
 
 export default async function RootLayout({ children }) {
   // fetch currently logged in user
   const { data: { userMeta, userProfile } = {} } = await getClient().query(
     GET_USER,
-    { userInput: null },
+    { userInput: null }
   );
   const user = { ...userMeta, ...userProfile };
 
