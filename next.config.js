c3e354db35c3e8a792c7af3960d189d5190cdb3d
Bhav Beri <bhavberi@gmail.com>
2024-09-06 13:22:11 +0530

    Mui is by default modularise (https://nextjs.org/docs/app/api-reference/next-config-js/optimizePackageImports)(https://github.com/vercel/next.js/pull/50900)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  modularizeImports: {
-    "@mui/icons-material": {
-      transform: "@mui/icons-material/{{member}}",
-    },
-  },
   images: {
     remotePatterns: [
       {
