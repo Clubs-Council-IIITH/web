67aee157de8d9ded0ebb19b1dfebb51d01aa4c79
Bhav Beri <bhavberi@gmail.com>
2024-11-30 21:18:06 +0530

    Move from adding favicon in head to normal nextjs way (https://nextjs.org/docs/app/building-your-application/upgrading/app-router-migration#step-3-migrating-nexthead)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -77,24 +77,6 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <meta name="apple-mobile-web-app-title" content="Life @ IIITH" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta charSet="utf-8" />
-      </head>
       <body className={fontClass}>
         <ModeProvider>
           <ThemeRegistry nonce={nonce}>
