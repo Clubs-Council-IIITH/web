f3c388fb803476220fe86e0c1fbe739591d44335
Bhav Beri <bhavberi@gmail.com>
2024-12-12 12:37:19 +0530

    Match the /student-bodies page with /clubs page(s). And handle error, redirect for student-body pages

--- src/app/clubs/[id]/page.jsx
+++ src/app/clubs/[id]/page.jsx
@@ -18,19 +18,27 @@ import MembersGrid from "components/members/MembersGrid";
 export async function generateMetadata({ params }) {
   const { id } = params;
 
+  let club;
+
   try {
-    const { data: { club } = {} } = await getClient().query(GET_CLUB, {
-      clubInput: { cid: id },
-    });
+    const { data: { club: fetchedClub } = {} } = await getClient().query(
+      GET_CLUB,
+      {
+        clubInput: { cid: id },
+      }
+    );
 
-    if (club?.studentBody) return permanentRedirect(`/student-bodies/${id}`);
-
-    return {
-      title: club.name,
-    };
+    club = fetchedClub;
   } catch (error) {
     notFound();
+    return;
   }
+
+  if (club?.studentBody) return permanentRedirect(`/student-bodies/${id}`);
+
+  return {
+    title: club.name,
+  };
 }
 
 export default async function Club({ params }) {
@@ -95,7 +103,9 @@ export default async function Club({ params }) {
               variant="none"
               color="secondary"
               component={Link}
-              href={`/clubs/${id}/members`}
+              href={`/${
+                club?.studentBody ? "student-bodies" : "clubs"
+              }/${id}/members`}
             >
               <Typography variant="button" color="text.primary">
                 View all
--- src/app/student-bodies/[id]/members/page.jsx
+++ src/app/student-bodies/[id]/members/page.jsx
@@ -1,32 +1,39 @@
 /*
 /* COPY OF `src/app/clubs/[id]/members/page.jsx`
- */
+*/
 
 import { getClient } from "gql/client";
 import { GET_CLUB } from "gql/queries/clubs";
+import { permanentRedirect, notFound } from "next/navigation";
 
-import { Box } from "@mui/material";
+import ClubMembers from "app/clubs/[id]/members/page";
 
-import MembersGrid from "components/members/MembersGrid";
-
-export async function generateMetadata({ params }, parent) {
+export async function generateMetadata({ params }) {
   const { id } = params;
 
-  const { data: { club } = {} } = await getClient().query(GET_CLUB, {
-    clubInput: { cid: id },
-  });
+  let club;
+
+  try {
+    const { data: { club: fetchedClub } = {} } = await getClient().query(
+      GET_CLUB,
+      {
+        clubInput: { cid: id },
+      }
+    );
+
+    club = fetchedClub;
+  } catch (error) {
+    notFound();
+    return;
+  }
+
+  if (!club?.studentBody) return permanentRedirect(`/clubs/${id}/members`);
 
   return {
     title: `Members | ${club.name}`,
   };
 }
 
-export default function Members({ params }) {
-  const { id } = params;
-
-  return (
-    <Box>
-      <MembersGrid clubid={id} />
-    </Box>
-  );
+export default function BodyMembers({ params }) {
+  return ClubMembers({ params });
 }
