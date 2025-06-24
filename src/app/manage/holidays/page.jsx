3d695598471c6d2bd75d5d6ad579fe73955eb06b
Dileepadari <adaridileep@gmail.com>
2025-06-25 03:09:20 +0530

    toggle add to filter past holidays

--- src/app/manage/holidays/page.jsx
+++ src/app/manage/holidays/page.jsx
@@ -1,43 +1,15 @@
-import Link from "next/link";
-
 import { getClient } from "gql/client";
 import { GET_HOLIDAYS } from "gql/queries/holidays";
 
-import { Container, Stack, Typography, Button } from "@mui/material";
-
-import HolidaysTable from "components/holidays/HolidaysTable";
-import Icon from "components/Icon";
+import ManageHolidaysClient from "components/holidays/ManageHolidaysClient";
 
 export const metadata = {
   title: "Manage Holidays",
 };
 
 export default async function ManageHolidays() {
-  const { data: { holidays } = {} } = await getClient().query(GET_HOLIDAYS);
+    const { data: { holidays } = {} } = await getClient().query(GET_HOLIDAYS);
 
-  return (
-    <Container>
-      <Stack
-        direction="row"
-        alignItems="center"
-        justifyContent="space-between"
-        mb={3}
-      >
-        <Typography variant="h3" gutterBottom>
-          Manage Holidays
-        </Typography>
-
-        <Button
-          component={Link}
-          href="/manage/holidays/new"
-          variant="contained"
-          startIcon={<Icon variant="add" />}
-        >
-          Add Holiday
-        </Button>
-      </Stack>
-
-      <HolidaysTable holidays={holidays} />
-    </Container>
-  );
+    return <ManageHolidaysClient holidays={holidays} />;
 }
+
