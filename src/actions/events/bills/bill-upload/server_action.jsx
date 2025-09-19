afd0b50bc3f65ce5655b2d99ea0c70e5bac60a72
Bhav Beri <bhavberi@gmail.com>
2025-09-19 13:56:16 +0530

    :sad:

--- src/actions/events/bills/bill-upload/server_action.jsx
+++ src/actions/events/bills/bill-upload/server_action.jsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { getClient } from "gql/client";
-import { ADD_BILL, REMIND_SLO } from "gql/mutations/events";
+import { ADD_BILL } from "gql/mutations/events";
 
 export async function eventBillUpload(details) {
   const response = { ok: false, data: null, error: null };
@@ -9,17 +9,6 @@ export async function eventBillUpload(details) {
   const { error } = await getClient().mutation(ADD_BILL, {
     details,
   });
-  if (error) {
-    response.error = {
-      title: error.name,
-      messages: error?.graphQLErrors?.map((ge) => ge?.message),
-    };
-  } else {
-    response.ok = true;
-  }
-
-  return response;
-
   if (error) {
     response.error = {
       title: error.name,
