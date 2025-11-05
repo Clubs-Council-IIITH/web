bd886d7cbce40af61e2d9f9609aa13a34dac5a97
Bhav Beri <bhavberi@gmail.com>
2025-11-06 00:06:09 +0530

    https://motion.dev/docs/react-upgrade-guide#12-0

--- src/components/TransitionProvider.jsx
+++ src/components/TransitionProvider.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 export default function TransitionProvider({ children }) {
   const pathname = usePathname();
