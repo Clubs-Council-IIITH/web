b9fd17ef090527a8d31c0fd0f4725078458c8f28
sed9871 <dashwinaravind1@gmail.com>
2026-04-03 21:28:47 +0530

    combining with any changes

--- src/components/ImageMasonry.jsx
+++ src/components/ImageMasonry.jsx
@@ -1,57 +1,31 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 
 import {
-  Box,
   Card,
   CardActionArea,
-  Grid,
   ImageList,
   ImageListItem,
-  Skeleton,
   useMediaQuery,
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 
 import ImageModal from "components/ImageModal";
-import { SettingsEthernet } from "@mui/icons-material";
 
 export default function ImageMasonry({ images, limit = undefined, cols = 4 }) {
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
 
   const [openImage, setOpenImage] = useState(null);
-  const [loadedImages, setLoadedImages] = useState(0);
   const totalImages = limit ? Math.min(images.length, limit) : images.length;
- 
-
-  const handleImageLoad = () => {
-    setLoadedImages((prev) => prev + 1);
-  };
-  const print_images=(images)=>{
-    console.log(images)
-  }
-  useEffect(()=>{
-    if(loadedImages<totalImages){
-      const timer = setTimeout(()=>{
-        setLoadedImages((prev)=>Math.min(prev+20, totalImages));
-        
-      }, 1000);
-
-
-    }
-  }, [loadedImages, totalImages]);
-
-
+  
   return (
     <>
-    
-    
       { (
         <ImageList variant="masonry" cols={isDesktop ? cols : 2} gap={10}>
-          {images.slice(0, totalImages).map((url, id) => {d
+          {images.slice(0, totalImages).map((url, id) => {
             return (
               <ImageListItem key={id}>
                 <Card
@@ -73,7 +47,7 @@ export default function ImageMasonry({ images, limit = undefined, cols = 4 }) {
                   <CardActionArea
                     onClick={() => {
                       setOpenImage(id);
-                      print_images(images);
+                  
                     }}
                     sx={{ lineHeight: 0 }}
                   >
@@ -82,7 +56,8 @@ export default function ImageMasonry({ images, limit = undefined, cols = 4 }) {
                       width={url.width|0}
                       height={url.height|0}
                       sizes="100vw"
-                      loading={id < loadedImages?"eager":"lazy"}
+                      preload={id==1}
+                      loading={id==1?"eager":"lazy"}
                       alt={`Gallery Image ${id}`}
                       style={{
                         width: "100%",
