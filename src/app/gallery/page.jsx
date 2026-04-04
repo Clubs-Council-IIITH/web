9fc0f6d1bc56c23da1d0c4cfeeccd61cd19d3c42
sed9871 <dashwinaravind1@gmail.com>
2026-04-05 01:30:59 +0530

    Changing next.config.json and formatting

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -12,10 +12,11 @@ export default async function Gallery({ limit = undefined }) {
 
 
   const galleryJSON = await response.json();
-  const galleryItems = galleryJSON["gallery"].map(
-    (item) => ({url:`${FILESERVER_URL}${item.url}`, height:item.height, width:item.width}),
-  );
-
+ galleryJSON["gallery"].map((item) => ({
+    url: `${FILESERVER_URL}${item.url}`,
+    height: item.height,
+    width: item.width,
+  }));
 
 
   return <ImageMasonry images={galleryItems} limit={limit} />;
